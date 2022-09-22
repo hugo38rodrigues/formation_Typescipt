f5ab6f93535f6212b4f3f92fdc56521820868a22
Xwdrblue <hugo38.rodrigues@gmail.com>
2022-09-22 08:49:15 +0200

    update for bug in h2

--- publication/src/components/PostDetail.tsx
+++ publication/src/components/PostDetail.tsx
@@ -7,7 +7,7 @@ const PostDetail:React.FC<PostdetailProps>=({onePost})=>{
     return (
         <div className="post">
             <h1>Publication numero: {onePost?.id}</h1>
-            <h2>Titre: {onePost.title}</h2>
+            <h2>Titre: {onePost?.title}</h2>
             <p>{onePost?.body}</p>
             <Link to="/">Page Principale</Link>
         </div>
